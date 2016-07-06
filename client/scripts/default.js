09562c6fa75a1cbaba6fab2d6675955dd9f5038d
unknown <daineka.pavel@gmail.com>
2016-07-06 17:45:02 +0300

    blocks highlited

--- client/scripts/default.js
+++ client/scripts/default.js
@@ -55,23 +55,70 @@
 
 var contentController = (function() {
 	var currentActive = null;
+	var defaultState = null;
+	var imageWidth = 0;
+	var imageHeight = 0;
+
+	getImagetNaturalSize();
+
 	return {
 		zoomInBlock: zoomInBlock,
 		zoomOut: clearLastActive
 	}
 
+
+
 	function zoomInBlock(blockId) {
 		clearLastActive();
+
 		var block = $('#container_' + blockId);
 		if (block) {
 			block.addClass("active-block");
 			currentActive = block;
 		}
 
+		var viewPort = $('.image-container');
+		defaultState = block.attr('style');
+		var height = viewPort.height()
+		var width = viewPort.width()
+
+
+		var image = $('#image-wrapper img');
+
+		var widthRatio = width / block.width();
+		var heightRatio = height / block.height();
+
+		if(image.height()*heightRatio>imageHeight)
+		{
+			heightRatio=imageHeight/image.height();
+		}
+		if(image.width()*widthRatio>imageWidth){
+			widthRatio=imageWidth/image.width();
+		}
+
+		block.height(block.height() * heightRatio);
+		block.width(block.width() * widthRatio);
+
+		image.height(image.height()*heightRatio);	
+
+
 	}
 
 	function clearLastActive() {
-		if (currentActive)
+		if (currentActive) {
 			currentActive.removeClass("active-block");
+			currentActive.attr('style', defaultState);
+			$('#image-wrapper img').attr('style', '');
+		}
+	}
+
+	function getImagetNaturalSize() {
+		var image = new Image();
+		image.src = $('#image-wrapper img').attr("src");
+		image.onload = function() {
+			imageWidth = this.width;
+			imageHeight = this.height;
+		};
+
 	}
 })();
